ec37f61dbed1784897db61b00900b35bf0d8b443
Aldi Riawan <54344657+aldiriawan@users.noreply.github.com>
2020-03-13 01:19:57 +0700

    Latihan Shared Preferences

--- app/src/main/java/com/example/helloworld/LoginActivity.java
+++ app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,10 +18,13 @@ public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     TextView tvDaftar;
 
+    SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         ptEmail = findViewById(R.id.ptEmail);
         ptPassword = findViewById(R.id.ptPassword);
         btnLogin = findViewById(R.id.btnLogin);
@@ -35,11 +39,23 @@ public class LoginActivity extends AppCompatActivity {
         });
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),
-                        "Login Sukses !", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                String ptEmailValue = ptEmail.getText().toString();
+                String ptPasswordValue = ptPassword.getText().toString();
+
+                String emailTerdaftar = preferences.getString("ptEmail", "");
+                String passwordTerdaftar = preferences.getString("ptPassword", "");
+
+                if(ptEmailValue.equals(emailTerdaftar) && ptPasswordValue.equals(passwordTerdaftar)){
+                    Toast.makeText(getApplicationContext(),
+                            "Login Sukses !", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),
+                            "Silahkan masukkan Email dan Password anda", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/helloworld/SignUpActivity.java
+++ app/src/main/java/com/example/helloworld/SignUpActivity.java
@@ -3,29 +3,55 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText ptEmail, ptPassword;
     Button btnSignUp;
+
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ptEmail = findViewById(R.id.ptEmail);
+        ptPassword = findViewById(R.id.ptPassword);
         btnSignUp = findViewById(R.id.btnSignUp);
+
+        preferences = getSharedPreferences("Userinfo", 0);
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),
-                        "Daftar Sukses!", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
-                startActivity(intent);
+                String ptEmailValue = ptEmail.getText().toString();
+                String ptPasswordValue = ptPassword.getText().toString();
+                if(ptEmailValue.length()>1){
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("ptEmail", ptEmailValue);
+                    editor.putString("ptPassword", ptPasswordValue);
+                    editor.apply();
+                    Toast.makeText(getApplicationContext(),
+                            "Daftar Sukses!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),
+                            "Isi setiap kolom yang ada!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+    public void emptyField(){
+        ptEmail.setText("");
+        ptPassword.setText("");
     }
 }
